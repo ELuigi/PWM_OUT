47cfcdc1010723c897d4157232701e0df0825377
Etienne LUIGI <ELG@softcomputing.corp>
2018-01-20 21:11:01 +0100

    Modify constructor argument to use PWM_OUT_PIN struct

--- lib/PWM_OUT/pwm_out.hpp
+++ lib/PWM_OUT/pwm_out.hpp
@@ -2,6 +2,7 @@
 #define __PWM_OUT_H
 
 #include "stm32f1xx_hal.h"
+#include "TARGET.hpp"
 class PWM_OUT
 {
 
@@ -9,7 +10,7 @@ private:
 
 public:
 
-PWM_OUT(uint16_t Pin);
+PWM_OUT(PWM_OUT_PIN Pinout);
 void SetFrequency(uint16_t Freq);
 void SetDutyCycle(float duty_cycle);
 };
