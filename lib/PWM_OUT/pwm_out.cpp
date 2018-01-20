bee91af81e1f4b28c8ecc8d861ac7780b2b3de30
Etienne LUIGI <ELG@softcomputing.corp>
2018-01-20 21:32:12 +0100

    add constructor fonction code

--- lib/PWM_OUT/pwm_out.cpp
+++ lib/PWM_OUT/pwm_out.cpp
@@ -1,1 +1,38 @@
 #include "pwm_out.hpp"
+
+
+PWM_OUT::PWM_OUT(PWM_OUT_PIN Pinout){
+    
+    TIM_OC_InitTypeDef sConfigOC;
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    htim->Instance = Pinout.TIMER;
+    htim->Init.Prescaler = 499;
+    htim->Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim->Init.Period = 999;
+    htim->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+
+    HAL_TIM_PWM_Init(htim);
+
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = 499;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+
+    HAL_TIM_PWM_ConfigChannel(htim,&sConfigOC,Pinout.CHANNEL);
+
+    if(htim->Instance==TIM3) {
+    __TIM3_CLK_ENABLE();
+
+    /**TIM3 GPIO Configuration
+    PA6     ------> TIM3_CH1
+    */
+    GPIO_InitStruct.Pin = Pinout.GPIO_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(Pinout.GPIO, &GPIO_InitStruct);
+
+    HAL_TIM_PWM_Start(htim,Pinout.CHANNEL);
+    __HAL_TIM_SET_AUTORELOAD(htim,500);
+}
