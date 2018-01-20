1c42ab6aa17fd1d70f7c7ec99ad47344d2956653
Etienne LUIGI <ELG@softcomputing.corp>
2018-01-20 21:10:14 +0100

    add info of pinout mapping

--- lib/TARGET/TARGET.hpp
+++ lib/TARGET/TARGET.hpp
@@ -5,11 +5,22 @@
 #include "stm32f1xx_hal.h"
 
 //create structur to contains all information needed to create pwm output
-struct Pwmout
+typedef struct PWM_OUT_PIN PWM_OUT_PIN;
+struct PWM_OUT_PIN
 {
   uint16_t GPIO_PPIN;
   TIM_TypeDef* TIMER;
   uint32_t CHANNEL;  
 };
+
+//liste of available pwm output
+
+//creation of all structs
+PWM_OUT_PIN pinout1 = {GPIO_PIN_6,TIM3,TIM_CHANNEL_1};
+PWM_OUT_PIN pinout2;
+PWM_OUT_PIN pinout3;
+PWM_OUT_PIN pinout4;
+
+
 
 #endif
